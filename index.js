7599bdd0fdcb2f6f9173fca7a02ff925823db05a
KHaaN007 <khaanmr007@gmail.com>
2023-10-18 07:06:07 +0600

    Add Product Post Function Added

--- index.js
+++ index.js
@@ -16,11 +16,52 @@ app.use(express.json());
 /**Their Will Be User Name & Password**/
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2wfm9qv.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+
+        const brandCollection = client.db("productDB").collection("product")
+
+
+
+
+        /**Connect the client to the server**/
+        app.post('/product', async (req, res) => {
+            const newproduct = req.body;
+            const result = await brandCollection.insertOne(newproduct)
+            res.send(result)
+        })
 
 
 
 
 
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 
